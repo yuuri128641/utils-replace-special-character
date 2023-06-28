7fbe651998d6a1bbe97d0a58e134e35d27f7654e
鈴木　優里 <yuri.suzuki@nijibox.co.jp>
2023-06-29 00:40:24 +0900

    fix characterList

--- utils/data/characterList.js
+++ utils/data/characterList.js
@@ -1,10 +1,550 @@
 module.exports.characterList = [
     {
+        searchCharacter: '«', 
+        changeCharacter: '&#171;'
+    },
+    {
+        searchCharacter: '»', 
+        changeCharacter: '&#187;'
+    },
+    {
+        searchCharacter: '<', 
+        changeCharacter: '&#60;'
+    },
+    {
+        searchCharacter: '>', 
+        changeCharacter: '&#62;'
+    },
+    {
+        searchCharacter: '∧', 
+        changeCharacter: '&#8743;'
+    },
+    {
+        searchCharacter: '∨', 
+        changeCharacter: '&#8744;'
+    },
+    {
+        searchCharacter: 'Ξ', 
+        changeCharacter: '&#926;'
+    },
+    {
+        searchCharacter: '×', 
+        changeCharacter: '&#215;'
+    },
+    {
+        searchCharacter: '←', 
+        changeCharacter: '&#8592;'
+    },
+    {
+        searchCharacter: '↑', 
+        changeCharacter: '&#8593;'
+    },
+    {
+        searchCharacter: '→', 
+        changeCharacter: '&#8594;'
+    },
+    {
+        searchCharacter: '↓', 
+        changeCharacter: '&#8595;'
+    },
+    {
+        searchCharacter: '¥', 
+        changeCharacter: '&#165;'
+    },
+    {
+        searchCharacter: '©', 
+        changeCharacter: '&#169;'
+    },
+    {
+        searchCharacter: '®', 
+        changeCharacter: '&#174;'
+    },
+    {
+        searchCharacter: '▲', 
+        changeCharacter: '&#9650;'
+    },
+    {
+        searchCharacter: '▼', 
+        changeCharacter: '&#9660;'
+    },
+    {
+        searchCharacter: '▶', 
+        changeCharacter: '&#9654;'
+    },
+    {
+        searchCharacter: '◀', 
+        changeCharacter: '&#9664;'
+    },
+    {
+        searchCharacter: '△', 
+        changeCharacter: '&#9651;'
+    },
+    {
+        searchCharacter: '▽', 
+        changeCharacter: '&#9661;'
+    },
+    {
+        searchCharacter: '▷', 
+        changeCharacter: '&#9655;'
+    },
+    {
+        searchCharacter: '◁', 
+        changeCharacter: '&#9665;'
+    },
+    {
+        searchCharacter: '■', 
+        changeCharacter: '&#9632;'
+    },
+    {
+        searchCharacter: '□', 
+        changeCharacter: '&#9633;'
+    },
+    {
+        searchCharacter: '◆', 
+        changeCharacter: '&#9670;'
+    },
+    {
+        searchCharacter: '◇', 
+        changeCharacter: '&#9671;'
+    },
+    {
+        searchCharacter: '○', 
+        changeCharacter: '&#9675;'
+    },
+    {
+        searchCharacter: '◎', 
+        changeCharacter: '&#9678;'
+    },
+    {
+        searchCharacter: '●', 
+        changeCharacter: '&#9679;'
+    },
+    {
+        searchCharacter: '•', 
+        changeCharacter: '&#8226;'
+    },
+    {
+        searchCharacter: '❇', 
+        changeCharacter: '&#10055;'
+    },
+    {
+        searchCharacter: '❈', 
+        changeCharacter: '&#10056;'
+    },
+    {
+        searchCharacter: '❉', 
+        changeCharacter: '&#10057;'
+    },
+    {
+        searchCharacter: '❊', 
+        changeCharacter: '&#10058;'
+    },
+    {
+        searchCharacter: '❋', 
+        changeCharacter: '&#10059;'
+    },
+    {
+        searchCharacter: '♠', 
+        changeCharacter: '&#9824;'
+    },
+    {
+        searchCharacter: '♣', 
+        changeCharacter: '&#9827;'
+    },
+    {
+        searchCharacter: '♥', 
+        changeCharacter: '&#9829;'
+    },
+    {
+        searchCharacter: '♦', 
+        changeCharacter: '&#9830;'
+    },
+    {
+        searchCharacter: '♤', 
+        changeCharacter: '&#9828;'
+    },
+    {
+        searchCharacter: '♧', 
+        changeCharacter: '&#9831;'
+    },
+    {
+        searchCharacter: '♡', 
+        changeCharacter: '&#9825;'
+    },
+    {
+        searchCharacter: '♢', 
+        changeCharacter: '&#9826;'
+    },
+    {
+        searchCharacter: '♨', 
+        changeCharacter: '&#9832;'
+    },
+    {
+        searchCharacter: '℃', 
+        changeCharacter: '&#8451;'
+    },
+    {
+        searchCharacter: 'ℓ', 
+        changeCharacter: '&#8467;'
+    },
+    {
+        searchCharacter: '∴', 
+        changeCharacter: '&#8756;'
+    },
+    {
+        searchCharacter: '∵', 
+        changeCharacter: '&#8757;'
+    },
+    {
+        searchCharacter: '¤', 
+        changeCharacter: '&#164;'
+    },
+    {
+        searchCharacter: '&', 
+        changeCharacter: '&#38;'
+    },
+    {
+        searchCharacter: '〒', 
+        changeCharacter: '&#12306;'
+    },
+    {
         searchCharacter: '①', 
         changeCharacter: '&#9312;'
     },
     {
         searchCharacter: '②', 
         changeCharacter: '&#9313;'
+    },
+    {
+        searchCharacter: '③', 
+        changeCharacter: '&#9314;'
+    },
+    {
+        searchCharacter: '④', 
+        changeCharacter: '&#9315;'
+    },
+    {
+        searchCharacter: '⑤', 
+        changeCharacter: '&#9316;'
+    },
+    {
+        searchCharacter: '⑥', 
+        changeCharacter: '&#9317;'
+    },
+    {
+        searchCharacter: '⑦', 
+        changeCharacter: '&#9318;'
+    },
+    {
+        searchCharacter: '⑧', 
+        changeCharacter: '&#9319;'
+    },
+    {
+        searchCharacter: '⑨', 
+        changeCharacter: '&#9320;'
+    },
+    {
+        searchCharacter: '⑩', 
+        changeCharacter: '&#9321;'
+    },
+    {
+        searchCharacter: 'Ⅰ', 
+        changeCharacter: '&#8544;'
+    },
+    {
+        searchCharacter: 'Ⅱ', 
+        changeCharacter: '&#8545;'
+    },
+    {
+        searchCharacter: 'Ⅲ', 
+        changeCharacter: '&#8546;'
+    },
+    {
+        searchCharacter: 'Ⅳ', 
+        changeCharacter: '&#8547;'
+    },
+    {
+        searchCharacter: 'Ⅴ', 
+        changeCharacter: '&#8548;'
+    },
+    {
+        searchCharacter: 'Ⅵ', 
+        changeCharacter: '&#8549;'
+    },
+    {
+        searchCharacter: 'Ⅶ', 
+        changeCharacter: '&#8550;'
+    },
+    {
+        searchCharacter: 'Ⅷ', 
+        changeCharacter: '&#8551;'
+    },
+    {
+        searchCharacter: 'Ⅸ', 
+        changeCharacter: '&#8552;'
+    },
+    {
+        searchCharacter: 'Ⅹ', 
+        changeCharacter: '&#8553;'
+    },
+    {
+        searchCharacter: '㎠', 
+        changeCharacter: '&#13216;'
+    },
+    {
+        searchCharacter: '㎡', 
+        changeCharacter: '&#13217;'
+    },
+    {
+        searchCharacter: '✓', 
+        changeCharacter: '&#10003;'
+    },
+    {
+        searchCharacter: '✔', 
+        changeCharacter: '&#10004;'
+    },
+    {
+        searchCharacter: '✕', 
+        changeCharacter: '&#10005;'
+    },
+    {
+        searchCharacter: '～', 
+        changeCharacter: '&sim;'
+    },
+    {
+        searchCharacter: '~', 
+        changeCharacter: '&sim;'
+    },
+    {
+        searchCharacter: '￥', 
+        changeCharacter: '&#65509;'
+    },
+    {
+        searchCharacter: '❶', 
+        changeCharacter: '&#10102;'
+    },
+    {
+        searchCharacter: '❷', 
+        changeCharacter: '&#10103;'
+    },
+    {
+        searchCharacter: '❸', 
+        changeCharacter: '&#10104;'
+    },
+    {
+        searchCharacter: '❹', 
+        changeCharacter: '&#10105;'
+    },
+    {
+        searchCharacter: '❺', 
+        changeCharacter: '&#10106;'
+    },
+    {
+        searchCharacter: '❼', 
+        changeCharacter: '&#10108;'
+    },
+    {
+        searchCharacter: '❽', 
+        changeCharacter: '&#10109;'
+    },
+    {
+        searchCharacter: '❾', 
+        changeCharacter: '&#10110;'
+    },
+    {
+        searchCharacter: '➓', 
+        changeCharacter: '&#10131;'
+    },
+    {
+        searchCharacter: '▶︎', 
+        changeCharacter: '&#9654;&#65038;'
+    },
+    {
+        searchCharacter: '㈱', 
+        changeCharacter: '&#12849;'
+    },
+    {
+        searchCharacter: '㈲', 
+        changeCharacter: '&#12850;'
+    },
+    {
+        searchCharacter: '㈹', 
+        changeCharacter: '&#12857;'
+    },
+    {
+        searchCharacter: '㍾', 
+        changeCharacter: '&#13182;'
+    },
+    {
+        searchCharacter: '㍽', 
+        changeCharacter: '&#13181;'
+    },
+    {
+        searchCharacter: '㍼', 
+        changeCharacter: '&#13180;'
+    },
+    {
+        searchCharacter: '㍻', 
+        changeCharacter: '&#13179;'
+    },
+    {
+        searchCharacter: '⑪', 
+        changeCharacter: '&#9322;'
+    },
+    {
+        searchCharacter: '⑫', 
+        changeCharacter: '&#9323;'
+    },
+    {
+        searchCharacter: '⑬', 
+        changeCharacter: '&#9324;'
+    },
+    {
+        searchCharacter: '⑭', 
+        changeCharacter: '&#9325;'
+    },
+    {
+        searchCharacter: '⑮', 
+        changeCharacter: '&#9326;'
+    },
+    {
+        searchCharacter: '⑯', 
+        changeCharacter: '&#9327;'
+    },
+    {
+        searchCharacter: '⑰', 
+        changeCharacter: '&#9328;'
+    },
+    {
+        searchCharacter: '⑱', 
+        changeCharacter: '&#9329;'
+    },
+    {
+        searchCharacter: '⑲', 
+        changeCharacter: '&#9330;'
+    },
+    {
+        searchCharacter: '⑳', 
+        changeCharacter: '&#9331;'
+    },
+    {
+        searchCharacter: '㎜', 
+        changeCharacter: '&#13212;'
+    },
+    {
+        searchCharacter: '㎝', 
+        changeCharacter: '&#13213;'
+    },
+    {
+        searchCharacter: '㎞', 
+        changeCharacter: '&#13214;'
+    },
+    {
+        searchCharacter: '㎎', 
+        changeCharacter: '&#13198;'
+    },
+    {
+        searchCharacter: '㎏', 
+        changeCharacter: '&#13199;'
+    },
+    {
+        searchCharacter: '㏄', 
+        changeCharacter: '&#13252;'
+    },
+    {
+        searchCharacter: '㍉', 
+        changeCharacter: '&#13129;'
+    },
+    {
+        searchCharacter: '㌔', 
+        changeCharacter: '&#13076;'
+    },
+    {
+        searchCharacter: '㌢', 
+        changeCharacter: '&#13090;'
+    },
+    {
+        searchCharacter: '㍍', 
+        changeCharacter: '&#13133;'
+    },
+    {
+        searchCharacter: '㌘', 
+        changeCharacter: '&#13080;'
+    },
+    {
+        searchCharacter: '㌧', 
+        changeCharacter: '&#13095;'
+    },
+    {
+        searchCharacter: '㌃', 
+        changeCharacter: '&#13059;'
+    },
+    {
+        searchCharacter: '㌶', 
+        changeCharacter: '&#13110;'
+    },
+    {
+        searchCharacter: '㍑', 
+        changeCharacter: '&#13137;'
+    },
+    {
+        searchCharacter: '㍗', 
+        changeCharacter: '&#13143;'
+    },
+    {
+        searchCharacter: '㌍', 
+        changeCharacter: '&#13069;'
+    },
+    {
+        searchCharacter: '㌦', 
+        changeCharacter: '&#13094;'
+    },
+    {
+        searchCharacter: '㌣', 
+        changeCharacter: '&#13091;'
+    },
+    {
+        searchCharacter: '㌫', 
+        changeCharacter: '&#13099;'
+    },
+    {
+        searchCharacter: '№', 
+        changeCharacter: '&#8470;'
+    },
+    {
+        searchCharacter: '℡', 
+        changeCharacter: '&#8481;'
+    },
+    {
+        searchCharacter: '№', 
+        changeCharacter: '&#8470;'
+    },
+    {
+        searchCharacter: '㏍', 
+        changeCharacter: '&#13261;'
+    },
+    {
+        searchCharacter: '℡', 
+        changeCharacter: '&#8481;'
+    },
+    {
+        searchCharacter: '㊤', 
+        changeCharacter: '&#12964;'
+    },
+    {
+        searchCharacter: '㊥', 
+        changeCharacter: '&#12965;'
+    },
+    {
+        searchCharacter: '㊦', 
+        changeCharacter: '&#12966;'
+    },
+    {
+        searchCharacter: '㊧', 
+        changeCharacter: '&#12967;'
+    },
+    {
+        searchCharacter: '㊨', 
+        changeCharacter: '&#12968;'
     },
 ]
